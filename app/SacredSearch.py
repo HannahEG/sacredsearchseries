7d16e73b1e8a077a387295097b80efd4026cc8dc
Hannah <66184980+HannahEG@users.noreply.github.com>
2021-03-26 16:07:45 -0600

    updated flask routes

--- app/SacredSearch.py
+++ app/SacredSearch.py
@@ -1,6 +1,5 @@
 #%%
-#Import Dependencies
-import pandas as pd
+#import dependencies
 from flask import Flask, render_template
 from flask import Flask, request
 
@@ -14,55 +13,32 @@ app = Flask(__name__)
 
 ## <ROUTES REFLECTING DIFFERENT WEBPAGES> 
 @app.route("/")
-def index():
-        return render_template ("index.html") 
+def home():
+        return render_template ("home.html") 
 #-------Route will return the webpage with turnout based on voter demographics: visualizations
 
 
-@app.route("/GeneralTurnout")
-def turnout():
-    return render_template ("turnout.html")
+@app.route("/Schedule")
+def Schedule():
+    return render_template ("schedule.html")
 
 
-@app.route("/Nonvoters")
+@app.route("/Faithstatement")
 #----- Route will return the webpage with reasons why people don't turn out: visualizations
-def visualize():
-    return render_template ("Nonvoters.html")
+def Faithstatement():
+    return render_template ("faithstatement.html")
 
 
-@app.route("/Competitiveness")
+@app.route("/Guidelines")
 #---- Route will return the webpage with visualizations depicting turnout based on voter competitiveness
-def visualize2():
-    return render_template("competitiveness.html")
+def Guidelines():
+    return render_template("guidelines.html")
 
 
-
-# Function to utilize saved RandomForestModel 
-def ValuePredictor(user_input): 
-    y_predict = np.array(user_input).reshape(1,5)
-    loaded_model = joblib.load("rf_final_model.pkl")
-    result = loaded_model.predict(y_predict) 
-    return result[0]
-    
-# Route to collect input data from user form to call model function and render prediction
-@app.route('/PredictTurnout', methods = ['GET','POST']) 
-def result():  
-    if request.method == 'POST': 
-        user_input = request.form.to_dict() 
-        user_input = OrderedDict(sorted(user_input.items(), key=lambda t: t[0]))
-        # return (user_input)
-        user_input = list(user_input.values()) 
-        user_input = list(map(float, user_input)) 
-        result = ValuePredictor(user_input)         
-        if result > 0: 
-            prediction = round(result * 100, 2)            
-        return render_template("model.html", prediction = prediction) 
-    return render_template('model.html')
- 
-@app.route("/OtherFeatures")
+@app.route("/Chapter1")
 #---- Route will return the webpage with visualizations depicting turnout based on voter competitiveness
-def visualize3():
-    return render_template("otherfeatures.html")
+def Chapter1():
+    return render_template("chapter1.html")
 
 
 if __name__ == "__main__":
